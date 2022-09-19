af22da9c7fe80a212de3682dc01203e82a3114e9
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-09-19 15:54:22 -0300

    Update aprendendo.js

--- aprendendo.js
+++ aprendendo.js
@@ -9,3 +9,46 @@ mongoose.connect('mongodb://localhost/banco', {
 }).catch((erro)=>{
     console.log('Houve um erro');
 })
+
+// Model - Usuarios
+
+    //Definindo o model
+    const UserSchema = mongoose.Schema({
+        nome:{
+            type: String,
+            require:false
+        },
+        sobrenome:{
+            type: String,
+            require:false
+        },
+
+        email:{
+            type: String,
+            require:false 
+        },
+        idade: {
+            type: Number,
+            require: true
+        },
+        pais:{
+            type: String
+        }
+
+
+})
+
+//Collection
+mongoose.model('User', UserSchema);
+
+new UserSchema({
+    nome:'Victor',
+    sobrenome: "Cesar",
+    email: 'Victor@',
+    idade: 22,
+    pais: "br"
+}).save().then(()=>{
+    console.log('Criado com sucesso');
+}).catch((err)=>{
+    console.log('Erro ao criar user');
+});
