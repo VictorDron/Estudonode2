87d31caf3b570c61d33400f7cb2d7cdfc5d6f602
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-09-19 16:43:54 -0300

    Update aprendendo.js

--- aprendendo.js
+++ aprendendo.js
@@ -1,19 +1,22 @@
 const mongoose = require ('mongoose');
 
 //Para conectar ao mongo
-mongoose.Promise = global.Promisse;
+mongoose.Promisse = global.Promisse;
 mongoose.connect('mongodb://localhost/banco', {
-    useMongoClient: true
+  
+    
+
 }).then(() => {
  console.log('Mongodb conectado');
 }).catch((erro)=>{
-    console.log('Houve um erro');
+    console.log('Houve um erro' + erro);
 })
 
 // Model - Usuarios
 
     //Definindo o model
     const UserSchema = mongoose.Schema({
+
         nome:{
             type: String,
             require:false
@@ -29,7 +32,7 @@ mongoose.connect('mongodb://localhost/banco', {
         },
         idade: {
             type: Number,
-            require: true
+            require: false
         },
         pais:{
             type: String
@@ -40,16 +43,19 @@ mongoose.connect('mongodb://localhost/banco', {
 
 //Collection
 mongoose.model('User', UserSchema);
+
 const Victor = mongoose.model('User');
 
-new UserSchema({
+new Victor ({
+
     nome:'Victor',
     sobrenome: "Cesar",
     email: 'Victor@',
     idade: 22,
     pais: "br"
+
 }).save().then(()=>{
     console.log('Criado com sucesso');
 }).catch((err)=>{
-    console.log('Erro ao criar user');
+    console.log('Erro ao criar user' + err);
 });
